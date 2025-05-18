b4bb0cce0e680dcda3012ecb206f2b92ede5e871
GIF1111 <alanmaeroff3@gmail.com>
2025-05-18 19:02:40 -0400

    section 3 done

--- docs/script.js
+++ docs/script.js
@@ -1,12 +1,23 @@
+console.log("script running");
 window.addEventListener('scroll', () => {
-    const section = document.querySelector('.section1');
-    const image = document.querySelector('.backImg');
+    const section1 = document.querySelector('.section1');
+    const image1 = document.querySelector('.backImg');
 
-    const sectionTop = section.offsetTop;
+    const sectionTop = section1.offsetTop;
     const scrollY = window.scrollY;
     const speed = 0.4;
 
     const offset = scrollY - sectionTop;
-    image.style.transform = `translateY(${offset * speed}px)`;
+    image1.style.transform = `translateY(${offset * speed}px)`;
 });
-console.log("script running");
+window.addEventListener('scroll', () => {
+    const section1 = document.querySelector('.section3');
+    const image1 = document.querySelector('.cataImg');
+
+    const sectionTop = section1.offsetTop;
+    const scrollY = window.scrollY;
+    const speed = 0.4;
+
+    const offset = scrollY - sectionTop;
+    image1.style.transform = `translateY(${offset * speed}px)`;
+});//<h4><a href="catalogue.html" class="btn1">CATALOGUE</a></h4>
